a1f8c05004aef5cf40d102d8f9c574c01062283d
T.Goehlert <t.goehlert@googlemail.com>
2025-01-26 01:41:09 +0100

    fix skytexture

--- src/texture.cpp
+++ src/texture.cpp
@@ -2,67 +2,68 @@
 
 namespace qformats::wad
 {
-    void QuakeTexture::FillTextureData(const uint8_t *buff, size_t size, bool flipHorizontal, const Palette &pal)
+void QuakeTexture::FillTextureData(const uint8_t *buff, size_t size, bool flipHorizontal, const Palette &pal)
+{
+    int k = 0, w = 0;
+    int h = flipHorizontal ? height - 1 : 0;
+    raw = cvec(size);
+    for (int idx = size - 1; idx >= 0; idx--)
     {
-        int k = 0, w = 0;
-        int h = flipHorizontal ? height - 1 : 0;
-        raw = cvec(size);
-        for (int idx = size - 1; idx >= 0; idx--)
+        auto rgba = pal.GetColor((int)(buff[w + (h * width)]));
+        raw[k] = rgba;
+        k++, w++;
+        if (w == width)
         {
-            auto rgba = pal.GetColor((int)(buff[w + (h * width)]));
-            raw[k] = rgba;
-            k++, w++;
-            if (w == width)
-            {
-                w = 0;
-                flipHorizontal ? h-- : h++;
-            }
+            w = 0;
+            flipHorizontal ? h-- : h++;
         }
-        return;
+    }
+    return;
+}
+
+void QuakeSkyTexture::FillTextureData(const uint8_t *buff, size_t size, bool flipHorizontal, const Palette &pal)
+{
+    width /= 2;
+    flipHorizontal = true;
+    type = wad::TTYPE_SKY_TEXTURE;
+    int k = 0, w = 0;
+    int h = flipHorizontal ? height - 1 : 0;
+    rawFront = cvec(size);
+    for (int idx = size - 1; idx >= 0; idx--)
+    {
+        auto rgba = pal.GetColor((int)(buff[w + (h * (width + width))]));
+        rawFront[k] = rgba;
+        k++, w++;
+        if (w == width)
+        {
+            w = 0;
+            flipHorizontal ? h-- : h++;
+        }
     }
 
-    void QuakeSkyTexture::FillTextureData(const uint8_t *buff, size_t size, bool flipHorizontal, const Palette &pal)
+    k = 0, w = width;
+    h = flipHorizontal ? height - 1 : 0;
+    raw = cvec(size);
+    for (int idx = size - 1; idx >= 0; idx--)
     {
-        width /= 2;
-        type = wad::TTYPE_SKY_TEXTURE;
-        int k = 0, w = 0;
-        int h = flipHorizontal ? height - 1 : 0;
-        rawFront = cvec(size);
-        for (int idx = size - 1; idx >= 0; idx--)
+        auto rgba = pal.GetColor((int)(buff[w + (h * (width + width))]));
+        raw[k] = rgba;
+        k++, w++;
+        if (w == (width + width))
         {
-            auto rgba = pal.GetColor((int)(buff[w + (h * width)]));
-            rawFront[k] = rgba;
-            k++, w++;
-            if (w == width)
-            {
-                w = 0;
-                flipHorizontal ? h-- : h++;
-            }
+            w = width;
+            flipHorizontal ? h-- : h++;
         }
+    }
+    return;
+}
 
-        k = 0, w = width;
-        h = flipHorizontal ? height - 1 : 0;
-        raw = cvec(size);
-        for (int idx = size - 1; idx >= 0; idx--)
-        {
-            auto rgba = pal.GetColor((int)(buff[w + (h * (width + width))]));
-            raw[k] = rgba;
-            k++, w++;
-            if (w == (width + width))
-            {
-                w = width;
-                flipHorizontal ? h-- : h++;
-            }
-        }
-        return;
-    }
-
-    const cvec &QuakeSkyTexture::BaseSky() const
-    {
-        return raw;
-    }
-    const cvec &QuakeSkyTexture::Alphaky() const
-    {
-        return raw;
-    }
+const cvec &QuakeSkyTexture::BaseSky() const
+{
+    return raw;
+}
+const cvec &QuakeSkyTexture::Alphaky() const
+{
+    return rawFront;
+}
 } // namespace qformats::wad
